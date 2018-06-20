21cbb31f8d548f4b549eaaf4bfeaac44443971bc
chloehaynes <19chaynes@nerinxhs.org>
2018-06-20 16:57:19 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -20,7 +20,7 @@ host_name = gets.chomp.capitalize
 
 puts "Dear #{guest_name},"
 puts ""
-puts "You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than October 30."
+puts "You are cordially invited to #{party_name} on #{date} at #{time}."
 puts ""
 puts "Sincerely,"
 puts ""
